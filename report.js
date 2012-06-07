042fcc2d8884f10b52fa0a5d41e454ba40d031cb
DTrejo <david.trejo@voxer.com>
2012-06-07 16:53:13 -0700

    latest request, working code :)

--- report.js
+++ report.js
@@ -1,7 +1,7 @@
 module.exports = report
 
 var request = require('request')
-var url = process.env.NPATURL || 'http://dtrejo.com/'; // TODO replace
+  , url = process.env.NPATURL || 'http://npat.iriscouch.com/results/'
 
 // reports test results to couchCB. additional information about the node
 // version and operating system is added before reporting.
@@ -30,14 +30,46 @@ function report(results, cb) {
   , platform: process.platform
   }
 
-  var options = { url: results.url || url, json: data }
-  
-  // TODO: remove me
-  console.log(options);
-  fs.writeFile(require('path').join(process.env.home, 'report.json')
-              , JSON.stringify(options)
-              , 'utf8')
-  // request.put(options, function(er, response, body) {
-  //   // TODO do something?
-  // })
+  var rand = uuid()
+  var uri = 'http://npat.iriscouch.com/results/' + rand;
+  var options = {
+    uri: uri
+  , json: data
+  }
+  request.put(options, function (error, response, body) {
+    if (response.statusCode == 201) {
+      console.log('   Test results saved at', uri, ' Thank you for participating :)')
+    } else {
+      console.log('   error: '+ response.statusCode)
+      console.log(body)
+    }
+  })
+}
+
+// via https://gist.github.com/982883, by
+function uuid(
+  a                  // placeholder
+){
+  return a           // if the placeholder was passed, return
+    ? (              // a random number from 0 to 15
+      a ^            // unless b is 8,
+      Math.random()  // in which case
+      * 16           // a random number from
+      >> a/4         // 8 to 11
+      ).toString(16) // in hexadecimal
+    : (              // or otherwise a concatenated string:
+      [1e7] +        // 10000000 +
+      -1e3 +         // -1000 +
+      -4e3 +         // -4000 +
+      -8e3 +         // -80000000 +
+      -1e11          // -100000000000,
+      ).replace(     // replacing
+        /[018]/g,    // zeroes, ones, and eights with
+        uuid         // random hex digits
+      )
+}
+
+// This works just fine
+if (require.main === module) {
+  report({ name: 'gss', stderr: 'sent via report.js ifmain' })
 }
